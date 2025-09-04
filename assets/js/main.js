6f56b4f5429ff5d564387019df1cf78d871acbd0
sahibakrp <sahiba.krp2000@gmail.com>
2025-09-04 13:32:37 +0530

    removed unused vender files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: NewBiz
-* Template URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
